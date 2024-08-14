9123bdc3b94fbc12ca2f390f2a894b4fa97cd844
JishnuVijayM <jishnupmd2002@gmail.com>
2024-08-14 17:45:31 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Login from './Pages/Login'
+import Register from './Pages/Register'
 
 function App() {
 
   return (
     <>
      <Login/>
+     <Register/>
     </>
   )
 }
