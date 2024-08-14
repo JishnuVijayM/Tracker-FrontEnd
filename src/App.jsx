57bac8c39b26b8812fa080e4ee02f57a4fe06190
JishnuVijayM <jishnupmd2002@gmail.com>
2024-08-14 17:36:21 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css'
+import Login from './Pages/Login'
+
+function App() {
+
+  return (
+    <>
+     <Login/>
+    </>
+  )
+}
+
+export default App
+
+// src/App.js
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+// import HomePage from './pages/HomePage';
+// import AboutPage from './pages/AboutPage';
+// import LoginPage from './pages/LoginPage';
+// import NotFoundPage from './pages/NotFoundPage';
+// import Navbar from './components/Navbar';
+
+// const App = () => {
+//   const isAuthenticated = !!localStorage.getItem('authToken'); // Check local storage for an auth token
+
+//   return (
+//     <Router>
+//       {isAuthenticated ? (
+//         <>
+//           <Navbar />
+//           <Switch>
+//             <Route exact path="/" component={HomePage} />
+//             <Route path="/about" component={AboutPage} />
+//             <Route component={NotFoundPage} />
+//           </Switch>
+//         </>
+//       ) : (
+//         <Redirect to="/login" />
+//       )}
+//     </Router>
+//   );
+// };
+
+// export default App;
