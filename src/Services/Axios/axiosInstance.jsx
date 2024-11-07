f8867b58ce7ae6e30081157412b9cff124a10ef5
JishnuVijayM <139347031+JishnuVijayM@users.noreply.github.com>
2024-11-07 17:29:05 +0530

    Update axiosInstance.jsx

--- src/Services/Axios/axiosInstance.jsx
+++ src/Services/Axios/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://tracker-backend-2j0k.onrender.com',
     timeout: 10000, 
     headers: {
         'Content-Type': 'application/json',
